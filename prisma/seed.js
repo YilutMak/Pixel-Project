47ae65e132d116f90345694adcf638026ebdc26f
yilutmak <yilutmak@gmail.com>
2022-10-26 15:08:53 +0800

    seed file change

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,9 @@
 import Client from '@prisma/client'
 import bcrypt from 'bcrypt'
 
-const prisma = new Client.PrismaClient()
+const prisma = new Client.PrismaClient({
+  log: ['info', 'query']
+})
 
 const seed = async () => {
   const foundUser = await prisma.user.upsert({
@@ -15,24 +17,21 @@ const seed = async () => {
   })
 
   await prisma.pixel.deleteMany({})
+  const dataToSave = []
   for (let y = 0; y < 100; y += 1) {
     for (let x = 0; x < 100; x += 1) {
-      // eslint-disable-next-line
-      await prisma.pixel.create({
-        data: {
-          x,
-          y,
-          RGB: '#FFFFFF',
-          user: {
-            connect: {
-              id: foundUser.id
-            }
-          }
-        }
+      dataToSave.push({
+        x,
+        y,
+        RGB: '#FFFFFF',
+        userId: foundUser.id
       })
-      console.log(`created x: ${x} | y: ${y}`)
+      console.log(`added ${y} ${x}`)
     }
   }
+  await prisma.pixel.createMany({
+    data: dataToSave
+  })
 }
 
 seed()
