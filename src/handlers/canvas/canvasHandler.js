fe58aef9d410a692d2b35f276b208c6b7ba3c785
yilutmak <yilutmak@gmail.com>
2022-10-27 16:18:28 +0800

    version 1.2 deploy

--- src/handlers/canvas/canvasHandler.js
+++ src/handlers/canvas/canvasHandler.js
@@ -1,24 +1,21 @@
 import prisma from '../../controllers/_helpers/prisma.js'
 
 const CanvasHandlers = (io, socket) => {
-  const receivePixel = (pixelData, color, currentUser) => {
+  const receivePixel = async (pixelData, color, currentUser) => {
     console.log(`${currentUser.user.username}: Pixel(x:${pixelData.x},y:${pixelData.y}) changed to ${color}`)
 
-    const pixelUpdate = async () => {
-      const dataToSave = {
-        x: pixelData.x,
-        y: pixelData.y,
-        timeStamp: new Date(Date.now()),
-        RGB: color,
-        userId: currentUser.user.id
-      }
-      await prisma.Pixel.create({ data: dataToSave })
+    const dataToSave = {
+      x: pixelData.x,
+      y: pixelData.y,
+      timeStamp: new Date(Date.now()),
+      RGB: color,
+      userId: currentUser.user.id
     }
+    const newPixelData = await prisma.Pixel.create({ data: dataToSave })
     // console.log(msg, user)
-    pixelUpdate(pixelData, color, currentUser)
     const timeStamp = new Date(Date.now())
     const UserTrigger = currentUser
-    io.emit('pixel updated', pixelData, color, UserTrigger, timeStamp)
+    io.emit('pixel updated', newPixelData, color, UserTrigger, timeStamp)
   }
 
   socket.on('pixel update', receivePixel)
